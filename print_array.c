9bc8b6232b50699173dd602a61a40398f3e16175
kevin10v <vokakevin@icloud.com>
2025-08-04 08:52:22 +0000

    Write a function that sorts an array of integers in ascending order using the Selection sort algorithm

--- print_array.c
+++ print_array.c
@@ -3,21 +3,18 @@
 
 /**
  * print_array - Prints an array of integers
- *
  * @array: The array to be printed
- * @size: Number of elements in @array
+ * @size: Number of elements in the array
  */
 void print_array(const int *array, size_t size)
 {
-    size_t i;
+	size_t i;
 
-    i = 0;
-    while (array && i < size)
-    {
-        if (i > 0)
-            printf(", ");
-        printf("%d", array[i]);
-        ++i;
-    }
-    printf("\n");
+	for (i = 0; i < size; i++)
+	{
+		if (i > 0)
+			printf(", ");
+		printf("%d", array[i]);
+	}
+	printf("\n");
 }
--- sort.h
+++ sort.h
@@ -1,24 +1,11 @@
 #ifndef SORT_H
 #define SORT_H
 
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
-/**
- * struct listint_s - Doubly linked list node structure
- * @n: Integer stored in the node
- * @prev: Pointer to the previous node
- * @next: Pointer to the next node
- */
-typedef struct listint_s
-{
-	const int n;
-	struct listint_s *prev;
-	struct listint_s *next;
-} listint_t;
 
-void print_list(const listint_t *list);
-void insertion_sort_list(listint_t **list);
-
+void print_array(const int *array, size_t size);
+void selection_sort(int *array, size_t size);
 
 #endif /* SORT_H */
